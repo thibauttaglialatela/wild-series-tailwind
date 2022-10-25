f10a4d4ed7ea45ce03df38d4d9c3383c4eead9e3
thibauttaglialatela <thibauttaglialatela@gmail.com>
2022-10-25 13:28:11 +0200

    create error 404 page + entity Category and Program

---
+++ src/Controller/ProgramController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/program', name: 'program_')]
+class ProgramController extends AbstractController
+{
+    #[Route('/', name: 'index')]
+    public function index(): Response
+    {
+        return $this->render('program/index.html.twig', [
+            'controller_name' => 'ProgramController',
+        ]);
+    }
+    #[Route('/{id}', name: 'show', requirements: ['id' => '\d+'], methods: ['GET'])]
+    public function show(int $id): Response
+    {
+        return $this->render('program/show.html.twig', [
+            'id' => $id,
+        ]);
+    }
+}
