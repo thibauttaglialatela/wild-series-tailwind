a6b584fcbb9265a35228e9a620de62180d72c5a6
thibauttaglialatela <thibauttaglialatela@gmail.com>
2022-10-26 17:13:57 +0200

    création d'une page catégorie/show avec les séries associés + mise en forme

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -3,10 +3,12 @@
 namespace App\Controller;
 
 use App\Repository\CategoryRepository;
+use App\Repository\ProgramRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-#[Route('/category', name:'category_')]
+
+#[Route('/category', name: 'category_')]
 class CategoryController extends AbstractController
 {
     #[Route('/', name: 'index')]
@@ -15,6 +17,23 @@ class CategoryController extends AbstractController
         $categories = $categoryRepository->findAll();
         return $this->render('category/_index.html.twig', [
             'categories' => $categories,
+        ]);
+    }
+
+    #[Route('/{categoryName}', name: 'show', methods: ['GET'])]
+    public function show(string $categoryName, CategoryRepository $categoryRepository, ProgramRepository $programRepository): Response
+    {
+        $category = $categoryRepository->findOneBy(['name' => $categoryName]);
+        if (!$category) {
+            throw $this->createNotFoundException(
+                'aucune catégorie de ce type trouvée'
+            );
+        }
+//récupére tous les programmes de cette catégorie
+        $programByCategory = $programRepository->findBy(['category' => $category]);
+        return $this->render('category/show.html.twig', [
+            'program_by_category' => $programByCategory,
+            'category' => $category,
         ]);
     }
 }
