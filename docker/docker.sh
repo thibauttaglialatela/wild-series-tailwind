1b8634fa6bdb0feff6b6566f3bca51c131590cdb
thibauttaglialatela <thibauttaglialatela@gmail.com>
2022-11-10 13:50:58 +0100

    sticky navbar OK

--- docker/docker.sh
+++ docker/docker.sh
@@ -2,4 +2,5 @@ composer require symfony/requirements-checker
 composer dump-env prod
 APP_ENV=prod APP_DEBUG=0 php bin/console cache:clear
 php bin/console d:m:m
+php bin/console d:f:l
 exec apache2-foreground
