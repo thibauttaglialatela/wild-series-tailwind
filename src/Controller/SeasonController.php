9a5082c40be0b3239d3e750482ebb49ea0ec74e2
thibauttaglialatela <thibauttaglialatela@gmail.com>
2022-11-24 17:22:45 +0100

    création du crud sur les saisons

---
+++ src/Controller/SeasonController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Program;
+use App\Entity\Season;
+use App\Form\SeasonType;
+use App\Repository\SeasonRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/season', name: 'season_')]
+class SeasonController extends AbstractController
+{
+    #[Route('/', name: 'index', methods: ['GET'])]
+    public function index(SeasonRepository $seasonRepository): Response
+    {
+        return $this->render('season/index.html.twig', [
+            'seasons' => $seasonRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'new', methods: ['GET', 'POST'])]
+    public function new(Request $request, SeasonRepository $seasonRepository): Response
+    {
+        $season = new Season();
+        $form = $this->createForm(SeasonType::class, $season);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $seasonRepository->save($season, true);
+
+            return $this->redirectToRoute('season_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('season/new.html.twig', [
+            'season' => $season,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'show', methods: ['GET'])]
+    public function show(Season $season): Response
+    {
+        return $this->render('season/show.html.twig', [
+            'season' => $season,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'edit', methods: ['GET', 'POST'])]
+
+    public function edit(Request $request, Season $season, SeasonRepository $seasonRepository): Response
+    {
+        $form = $this->createForm(SeasonType::class, $season);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $seasonRepository->save($season, true);
+
+            return $this->redirectToRoute('season_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('season/edit.html.twig', [
+            'season' => $season,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'delete', methods: ['POST'])]
+    public function delete(Request $request, Season $season, SeasonRepository $seasonRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$season->getId(), $request->request->get('_token'))) {
+            $seasonRepository->remove($season, true);
+        }
+
+        return $this->redirectToRoute('app_season_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
---
+++ src/Form/SeasonType.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Season;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SeasonType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('number', NumberType::class, [
+                'label' => 'Numéro de la saison',
+                'label_attr' => ['class' => 'Text-white Text-medium'],
+                'attr' => ['class' => 'text-black md:text-xl w-full']
+            ])
+            ->add('year', NumberType::class, [
+                'label' => 'Année',
+                'label_attr' => ['class' => 'Text-white Text-medium'],
+                'attr' => ['class' => 'text-black md:text-xl w-full']
+
+            ])
+            ->add('description', TextareaType::class, [
+                'label' => 'résumé de la saison',
+                'label_attr' => ['class' => 'Text-white Text-medium'],
+                'attr' => ['row' => 5, 'col' => 10, 'class' => 'text-black md:text-xl w-full']
+            ])
+            ->add('program', null, [
+                'choice_label' => 'title',
+                'label' => 'Série associée',
+                'label_attr' => ['class' => 'Text-white Text-medium'],
+                'attr' => ['class' => 'text-black md:text-xl w-full']
+                ],)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Season::class,
+        ]);
+    }
+}
