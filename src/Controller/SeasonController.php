1f755dc3d4cd777401ff80cd99617262571c8b3a
thibauttaglialatela <thibauttaglialatela@gmail.com>
2022-12-14 17:33:58 +0100

    ajout message flash de création et suppression d'une saison

--- src/Controller/SeasonController.php
+++ src/Controller/SeasonController.php
@@ -33,6 +33,7 @@ class SeasonController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $season->setProgram($program);
             $seasonRepository->save($season, true);
+            $this->addFlash('green', 'Une saison a bien été enregistrée.');
 
             return $this->redirectToRoute('program_show', ['id' => $program->getId()], Response::HTTP_SEE_OTHER);
         }
@@ -81,6 +82,7 @@ class SeasonController extends AbstractController
 
         if ($this->isCsrfTokenValid('delete'.$season->getId(), $request->request->get('_token'))) {
             $seasonRepository->remove($season, true);
+            $this->addFlash('red', 'La saison a été supprimé !');
         }
 
         return $this->redirectToRoute('program_show', ['id' => $season->getProgram()->getId()], Response::HTTP_SEE_OTHER);
