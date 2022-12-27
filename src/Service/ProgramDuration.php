1565e755ac54f5456c7f44ca4b2f30ce55b6b969
thibauttaglialatela <thibauttaglialatela@gmail.com>
2022-12-27 17:52:14 +0100

    rajout de la durée totale de visionnage sur chaque fiche série

--- src/Service/ProgramDuration.php
+++ src/Service/ProgramDuration.php
@@ -6,9 +6,31 @@ use App\Entity\Program;
 
 class ProgramDuration
 {
-
-    public function calculate(Program $program):string
+    public function calculate(Program $program): string
     {
-        return "Coming soon";
+        $numberOfSeasons = $program->getSeasons()->count();
+        $totalWatchingTime = 0;
+        for ($i = 0; $i < $numberOfSeasons; $i++) {
+            for ($j = 0; $j < $program->getSeasons()[$i]->getEpisodes()->count(); $j++) {
+                $totalWatchingTime += $program->getSeasons()[$i]->getEpisodes()[$j]->getDuration();
+            }
+        }
+        return $this->convertMinToDays($totalWatchingTime);
+    }
+
+    public function convertMinToDays($min)
+    {
+        $hours = str_pad(floor($min / 60), 2, "0", STR_PAD_LEFT);
+        $mins = str_pad($min % 60, 2, "0", STR_PAD_LEFT);
+
+        if ((int)$hours > 24) {
+            $days = str_pad(floor($hours / 24), 2, "0", STR_PAD_LEFT);
+            $hours = str_pad($hours % 24, 2, "0", STR_PAD_LEFT);
+        }
+        if (isset($days)) {
+            $days = $days . " jour(s) ";
+        }
+
+        return $days . $hours . " Heure(s) " . $mins . " minute(s)";
     }
 }
