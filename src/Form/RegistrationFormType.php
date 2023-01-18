56d9e233d2b84d63ab259338c78e8d20377faf4b
thibauttaglialatela <thibauttaglialatela@gmail.com>
2023-01-18 11:49:18 +0100

    ajout du prénom et du nom à la fiche utilisateur

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -7,6 +7,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
@@ -19,6 +20,16 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
+            ->add('firstname', TextType::class, [
+                'label' => 'Prénom',
+                'label_attr' => ['class' => 'text-white md:text-2xl font-serif font-bold'],
+                'attr' => ['class' => 'text-black md:text-xl w-full']
+            ])
+            ->add('lastname', TextType::class, [
+                'label' => 'Nom',
+                'label_attr' => ['class' => 'text-white md:text-2xl font-serif font-bold'],
+                'attr' => ['class' => 'text-black md:text-xl w-full']
+            ])
             ->add('email', EmailType::class, [
                 'label' => 'e-mail',
                 'label_attr' => ['class' => 'text-white md:text-2xl font-serif font-bold'],
