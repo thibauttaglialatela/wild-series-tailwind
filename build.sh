35dcae2c653e6cbc1628964a5c1b32f8358f50bc
thibauttaglialatela <thibauttaglialatela@gmail.com>
2023-03-14 17:32:58 +0100

    MAJ build

--- build.sh
+++ build.sh
@@ -4,7 +4,8 @@ rm -Rf vendor/
 composer install --ignore-platform-reqs
 yarn install
 composer dump-env prod
-php bin/console doctrine:migrations:migrate -f
+php bin/console doctrine:schema:create
+php bin/console doctrine:migrations:migrate
 php bin/console cache:clear
 php bin/console cache:warmup
 yarn build
